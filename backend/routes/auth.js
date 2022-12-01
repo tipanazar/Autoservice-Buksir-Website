53f2b851947f8975167f91afd5df6a35dc286506
Nazar <karpeko76@gmail.com>
2022-12-01 23:18:56 +0000

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -25,8 +25,6 @@ router.post("/signin", async (req, res, next) => {
     }
     const token = jwt.sign(admin._id, SECRET_KEY, { expiresIn: "5h" });
     console.log(token);
-    // const all = await Admin.findById({ _id: "6388c6156588dd3cadc0bfc6" });
-    // console.log(all)
     res.status(200).json({});
   } catch (err) {
     next(err);
